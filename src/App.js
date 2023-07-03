823cc4f217315c9a2c7c881fd80b906c9c5a61ce
Manthan Indane <manthanindane141@gmail.com>
2023-07-03 18:13:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Calender from "./Calender";
+import Dashboard from "./Dashboard";
+import Leavehistory from "./Leavehistory";
+import Login from "./Login";
+import Mainpage from "./Mainpage";
+import Notifications from "./Notifications";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import EmployeeRecords from "./EmployeeRecords";
+// import { Route } from 'react-router-dom';
+
+// import Create from "./Create";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Login/>} />
+          <Route path="/Login" element={<Login/>} />
+          <Route path="/Dashboard" element={<Dashboard/>} />
+          <Route path="/Leavehistory" element={<Leavehistory/>} />
+          <Route path="/Calender" element={<Calender/>} />
+          <Route path="/Notifications" element={<Notifications/>} />
+          <Route path="/EmployeeRecords" element={<EmployeeRecords/>} />
+          
+          {/* <Route path="/Create" element={<Create/>} /> */}
+          
+          
+
+          <Route path="*" element={<> not found</>} />
+        </Routes>
+      </Sidebar>
+    </Router>
   );
 }
 
